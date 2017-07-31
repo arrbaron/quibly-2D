76b51d7858121291040126e1ab315ffa3a8118e1
Roy <baron.roy@gmail.com>
2017-07-31 15:41:14 -0400

    Added builds folder and menu

---
+++ _builds/7-31-17/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1501529077,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/quibly-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/gun-sheet0.png",
+		"images/shield-sheet0.png",
+		"images/reticle-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/killzone-sheet0.png",
+		"images/healthbar-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"quibly stage 1 - 2048 (lighter).png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
